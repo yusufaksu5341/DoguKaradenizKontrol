254eda066202c3786bdf56228d8659f90e6fec90
yusufaksu5341 <120259133+yusufaksu5341@users.noreply.github.com>
2025-03-16 17:24:33 +0300

    doguKaradenizKontrol.c

--- DoguKaradenizKontrol/doguKaradenizKontrol.c
+++ DoguKaradenizKontrol/doguKaradenizKontrol.c
@@ -2,33 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-void checkCode(int kod) {
-    switch (kod) {
-        case 8:
-        case 28:
-        case 29:
-        case 52:
-        case 53:
-        case 61:
-        case 69:
-            printf("Doğu Karadeniz bölgesinde yer alır\n");
+void checkCode(int kod, int *plakaKodlari, int size) {
+    int found = 0;
+    for (int i = 0; i < size; i++) {
+        if (plakaKodlari[i] == kod) {
+            found = 1;
             break;
-        default:
-            if (kod > 81 || kod < 1) {
-                printf("Geçerli Bir Plaka Kodu giriniz\n");
-            } else {
-                printf("Doğu Karadeniz bölgesinde yer almaz\n");
-            }
-            break;
+        }
+    }
+    if (found) {
+        printf("Doğu Karadeniz bölgesinde yer alır\n");
+    } else {
+        if (kod > 81 || kod < 1) {
+            printf("Geçerli Bir Plaka Kodu Giriniz\n");
+        } else {
+            printf("Doğu Karadeniz bölgesinde yer almaz\n");
+        }
     }
 }
 
 int main() {
+    FILE *file;
     int kod;
+    int plakaKodlari[100];
+    int size = 0;
+
+    file = fopen("trafik_kodlari.txt", "r");
+    if (file == NULL) {
+        printf("Dosya açılamadı\n");
+        return 1;
+    }
+
+    while (fscanf(file, "%d", &plakaKodlari[size]) != EOF) {
+        size++;
+    }
+    fclose(file);
 
     printf("Plaka Kodu Giriniz= ");
     scanf("%d", &kod);
-    checkCode(kod);
+    checkCode(kod, plakaKodlari, size);
 
     return 0;
 }
